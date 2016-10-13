427a258b74149e827684765e9aee0821e2fa56b8
Generic Error <GenericError@users.noreply.github.com>
2016-10-13 11:23:23 +1100

    First commit for rmdir.py
    First commit for the rmdir command.

---
+++ rmdir.py
@@ -0,0 +1,27 @@
+import os
+
+def run_command(arguments={}):
+    try:
+        current_dir = arguments['cwd']
+    except:
+        print("Sorry an error occured.")
+        return
+
+    try:
+        extra_input = arguments['extra_input']
+    except:
+        print("Error: no directory name supplied.")
+        return
+
+    if extra_input == "":
+        print("Error: no directory name supplied.")
+        return
+
+    try:
+        os.rmdir(path=extra_input)
+    except OSError:
+        print("The directory", extra_input, "is not empty.")
+        return
+    except:
+        print("Sorry, an error occured.")
+        return
