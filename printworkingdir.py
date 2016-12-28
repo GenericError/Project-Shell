cccb1715a3d5cf7f312431cc629fec52eb25af66
Generic Error <GenericError@users.noreply.github.com>
2016-12-29 09:49:10 +1100

    Updated pwd to use argsplit & shellexceptions
    Updated the ls command (module) to understand argsplit output and to use the custom exceptions as defined in the shellexceptions module

--- printworkingdir.py
+++ printworkingdir.py
@@ -2,6 +2,15 @@
 
 import os
 
-def run_command(processed_string):
+def run_command(argument_list):
     """ Function that runs the pwd command """
+    amount_required = 0
+    try:
+        if len(argument_list) >= amount_required:
+            pass
+        else:
+            raise Exception
+    except:
+        print("error: a required flag or argument was missing")
+        return
     print(os.path.abspath(os.getcwd()))
