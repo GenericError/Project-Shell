03e43f1be7a58615ee93b7654abe82c836405548
Generic Error <GenericError@users.noreply.github.com>
2016-12-28 19:23:49 +1100

    Updated cd to use argsplit & shellexceptions
    Updated the cd command (module) to understand argsplit output and to use the custom exceptions as defined in the shellexceptions module

--- cd.py
+++ cd.py
@@ -1,33 +1,39 @@
 """ Module that executes the cd command """
 import os  # Importing this module to do directory things
+import argparse
+from shellexceptions import *
 
-
-def run_command(arguments):
+def run_command(argument_list):
     """ Runs the cd command """
-    extra_input = ""  # Set the dir to cd into to a blank string
-    current_dir = os.getcwd()  # Current direcotry is fetched by the system
-    try:  # Try to do the following
-        # Get the extra input required. In this case,
-        # it is the directory to cd into
-        extra_input = arguments['extra_input']
-    except KeyError:  # In case the extra data was not in the dictionary
-        # Tell the user that they missed out the argument
-        print("The location to change to is the required argument")
-        return None  # Go back to the prompt
-    # Get the path of the directory to change in to
-    directory_to_go = os.path.join(current_dir, extra_input)
-    if extra_input == "":  # If the user put in a blank directory
-        print("Directoy can not be blank.")  # Tell the user
-        return None  # Go back to the prompt
-    elif extra_input == "~":  # If the user wants to go to the home folder
-        os.chdir(os.path.expanduser('~'))  # Goes to the folder
-    # If the argument does not give a valid directory
-    elif not os.path.isdir(directory_to_go):
-        print(extra_input, "is not a directory.")  # Tell the user
-        return None  # Go back to the prompt
-    elif os.path.isdir(directory_to_go):  # If it is a valid directory
+    amount_required = 1
+    try:
+        if len(argument_list) >= amount_required:
+            pass
+        else:
+            raise Exception
+    except:
+        print("error: a required flag or argument was missing")
+        return
+    new_directory = ""
+    for i in argument_list:
+        if not i.startswith("-"):
+            new_directory = i
+            break
+    directory_to_go = os.path.join(os.getcwd(), new_directory)
+    try:
+        if new_directory == "":  # If the user put in a nothing or a space
+            raise BlankDirectoryException
+        elif new_directory == "~":  # If the user wants to go to the home folder
+            os.chdir(os.path.expanduser('~'))  # Goes to the folder
+        # If the argument does not give a valid directory
+        elif not os.path.isdir(directory_to_go):
+            raise NotADirectoryException(new_directory)
+    except GenericException as e:
+        print(e.error_message)
+    except BlankDirectoryException as e:
+        print(e.error_message)
+    except NotADirectoryException as e:
+        print(e.error_message)
+    if os.path.isdir(directory_to_go):  # If it is a valid directory
         os.chdir(directory_to_go)  # Change the current directory
-    else:  # Otherwise
-        print("Sorry, an error occured.")  # Tell the user there was an error
-        return None  # Go back to the prompt
     return None  # Go back to the prompt
