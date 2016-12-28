73dbb2ce53c450433e87cbee4226bed968a9ceaf
Generic Error <GenericError@users.noreply.github.com>
2016-12-28 20:46:23 +1100

    Updated rmdir to use argsplit & shellexceptions
    Updated the rmdir command (module) to understand argsplit output and to use the custom exceptions as defined in the shellexceptions module

--- rmdir.py
+++ rmdir.py
@@ -1,25 +1,32 @@
 """ Module which includes the rmdir command """
+
 import os  # Importing this for deletion and directory things
+from shellexceptions import *
 
-
-def run_command(arguments):
+def run_command(argument_list):
     """ Function which runs the rmdir command"""
-    try:  # Try to do the following
-        # Get the name of the directory to delete
-        extra_input = arguments['extra_input']
-    except KeyError:  # If a name wasn't supplied
-        print("Error: no directory name supplied.")  # Tell the user
-        return None  # Go back to the prompt
+    amount_required = 1
+    try:
+        if len(argument_list) >= amount_required:
+            pass
+        else:
+            raise Exception
+    except:
+        print("error: a required flag or argument was missing")
+        return
 
-    if extra_input == "":  # If the name supplied is blank
-        print("Error: no directory name supplied.")  # Tell the user
-        return None  # Go back to the prompt
+    directory_to_remove = ""
+    for i in argument_list:
+        if not i.startswith("-"):
+            directory_to_remove = i
+            break
 
     try:  # Try to do the follwing
-        os.rmdir(path=extra_input)  # Remove the directory
-    except OSError:  # If the directory isn't empty
-        print("The directory", extra_input, "is not empty.")  # Tell the user
-        return None  # Go back to the prompt
-    except Exception:  # If another error occured
-        print("Sorry, an error occured.")  # Tell the user
-        return None  # Go back to the prompt
+        os.rmdir(path=directory_to_remove)  # Remove the directory
+    except OSError as e:
+        raise DirectoryNotEmptyException
+    except DirectoryNotEmptyException as e:
+        e.print_error()
+    except Exception as e:
+        print("Hmmm, something went wrong?")
+    return None
