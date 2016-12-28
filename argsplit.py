dd5b30dc1bd8553fb979f41f8d4d88605b7527d4
Generic Error <GenericError@users.noreply.github.com>
2016-12-28 18:25:01 +1100

    Added argument splitting module
    Added a dedicated module which contains one function, a function which processes a string as set out in the testing code

---
+++ argsplit.py
@@ -0,0 +1,25 @@
+""" argsplit - splits a string in the style of sys.argv """
+
+import sys
+
+def process_string(string_to_process):
+    """ Returns a list of flags and arguments from the string supplied """
+    ESCAPE_TEXT = "$ESCAPE$"
+
+    string_to_process = string_to_process.replace("\\"+" ", ESCAPE_TEXT)
+    processed_list = string_to_process.split(" ")
+    incrementer = 0
+
+    while incrementer < len(processed_list):
+        new_string = str(processed_list[incrementer]).replace(ESCAPE_TEXT, " ")
+        processed_list[incrementer] = new_string
+        incrementer += 1
+
+    processed_list.pop(0)  # Remove the command name
+    return processed_list
+
+if sys.argv[1] == "--test":
+    test_string_to_process = input("The test string > ")
+    test_processed_list = process_string(test_string_to_process)
+    print("Here is the list returned by the function:")
+    print(test_processed_list)
