20c541976f041cbe4d5298da63d186083fd68f5e
Generic Error <GenericError@users.noreply.github.com>
2016-12-28 20:08:06 +1100

    Updated cp to use argsplit & shell exceptions
    Updated the cp command (module) to understand argsplit output and to use the custom exceptions as defined in the shellexceptions module

--- cp.py
+++ cp.py
@@ -2,51 +2,59 @@
 
 import shutil  # Import this for copyying files with metadata
 import os  # Import this for system things
+from shellexceptions import *
 
 
-def run_command(arguments):
+def run_command(argument_list):
     """ Function that runs the acutal command based on the user's input """
+    amount_required = 2
 
-    # If the user supplied less than two arguments
-    if len(arguments['more_input']) < 2:
-        # Tell the user that they did so
-        print("Source and/or destination arguments are required")
-        return None  # Go back to the prompt
-    # If the user supplied more than two arguments
-    elif len(arguments['more_input']) > 2:
-        # Tell the user that they did so
-        print("Source and destination are the only required arguments")
-        return None  # Go back to the prompt
+    try:
+        if len(argument_list) >= amount_required:
+            pass
+        else:
+            raise Exception
+    except:
+        print("error: a required flag or argument was missing")
+        return
 
-    more_input = arguments['more_input']  # Grab the names of the directories
-    source = more_input[0]  # Grab the source file name
-    destination = more_input[1]  # Grab the destination file name
+    source = None
+    destination = None
+    for i in argument_list:
+        if not i.startswith("-"):
+            if source is None:
+                source = i
+            else:
+                destination = i
 
-    if source == destination:  # If the source and the destination are the same
-        print("The two given arguments can not be the same")  # Tell the user
-        return None  # Go back to the prompt
-    elif os.path.isdir(source):  # Im the source argument is a directory
-        print("Error: the source argumnet is a directory!")  # Tell the user
-        return None  # Go back to the prompt
-    elif os.path.isfile(source):  # If the source is a file
-        if os.path.exists(destination):  # If the destination exists
+    try:
+        if source == destination:  # If the source and the destination are the same
+            raise SourceDestinationAreEqualException
+        elif os.path.isdir(source):  # Im the source argument is a directory
+            raise SourceArgumentIsADirectoryException
+        elif os.path.isfile(source):  # If the source is a file
+            if os.path.exists(destination):  # If the destination exists
+                # TODO: Implement the appropriate actions here
+                raise UnsupportedOperationException("cp")
+            # If the destination does not exist
+            elif not os.path.exists(destination):
+                try:  # Try to do the following
+                    # Copy the source to the destination with metadata
+                    shutil.copy2(source, destination)
+                except OSError:  # In case an error occured
+                    raise GenericException
+            else:  # In case something went wrong
+                raise GenericException
+        else:  # In case the user tried to perform another operation
             # TODO: Implement the appropriate actions here
-            # Tell the user that this function is not currently supported
-            print("That operation is not currently supported by cp")
-            return None  # Go back to the prompt
-        # If the destination does not exist
-        elif not os.path.exists(destination):
-            try:  # Try to do the following
-                # Copy the source to the destination with metadata
-                shutil.copy2(source, destination)
-            except OSError:  # In case an error occured
-                print("Sorry, and error occured.")  # Tell the user
-                return None  # Go back to the prompt
-        else:  # In case something went wrong
-            print("Sorry an error occured.")  # Tell the user
-            return None  # Go back to the prompt
-    else:  # In case the user tried to perform another operation
-        # TODO: Implement the appropriate actions here
-        # Tell the user the operation is not supported
-        print("That operation is not currently supported by cp")
-        return None  # Go back to the prompt
+            raise UnsupportedOperationException("cp")
+        return None
+    except UnsupportedOperationException as e:
+        print(e.error_message)
+    except GenericException as e:
+        print(e.error_message)
+    except SourceDestinationAreEqualException as e:
+        print(e.error_message)
+    except SourceArgumentIsADirectoryException as e:
+        print(e.error_message)
+    return None
