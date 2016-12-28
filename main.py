bb696c008a7f9681c699165615a2f085fbc1204c
Generic Error <GenericError@users.noreply.github.com>
2016-12-28 19:30:33 +1100

    Major code removal
    Removed a major part of the code in the main module, which is now unnecessary since the introduction of the argsplit module. The main module now uses argsplit to process the input from the user.

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ try:
     import rmdir
     import cp
     import rm
+    import argsplit
 except ImportError:  # If any module failed to be imported
     print("Sorry, Project Shell can not run without the required modules!")
+    print("This might have been a random error, perhaps try running")
+    print("Project Shell again to see if it happens twice.")
     exit()  # Tell the user and exit the program
 
 NEWLINE = "\n"  # Constant for the newline character (\n)
@@ -100,68 +103,10 @@ while 1:  # Main event loop
     PRECEDING_TEXT += "$ "  # Append a dollar sign and a space
     COMMAND_INPUT = str(input(PRECEDING_TEXT))  # Get the input of the user
     # Split the command and its flags and aguments, splitting them at spaces
-    JUST_COMMAND = COMMAND_INPUT.split(' ')[0].lower()
-    try:
-        # Try to see if there is more input other than just the command
-        AFTER_COMMAND = COMMAND_INPUT.split(' ')[1]
-    except IndexError:  # If the command is the only input
-        AFTER_COMMAND = ""  # A blank string will be the extra input
-    for command in COMMAND_DICT:  # For each command in the dictionary
+    JUST_COMMAND = COMMAND_INPUT.split(' ')[0].lower()for command in COMMAND_DICT:  # For each command in the dictionary
         if command == JUST_COMMAND:  # If the command is equal to the input
             func = COMMAND_DICT[command]
-            arguments = {}  # Initialise an empty dictionary for arguments
-            # For each command in the argument dictionary
-            for thing in ARGS_DICT:
-                # For each argument in the arguments dictionary that is
-                # required for the specific command
-                for i in ARGS_DICT[command]:
-                    if i == 'extra_input':  # If the argument is extra_input
-                        arguments['extra_input'] = AFTER_COMMAND  # Add to dict
-                    elif i == 'more_input':  # If the argument is more_input
-                        # Generate that extra input and add it to the dict
-                        arguments['more_input'] = gen_more_input(COMMAND_INPUT)
-            # Run the command and capture its restlt
-            func_result = func(arguments)
-            # If the function returns None, then it has run successfully,
-            # meaning we can return to the prompt and ask for the next command
-            if func_result is None:
-                continue  # Jump to the next iteration of the main loop
-            else:  # Otherwise
-                while 1:  # Repeat whilst the number one is True
-                    try:  # Try to do the following
-                        # If the command needs more input, it will return a
-                        # dictionary with its requirements. We will then run
-                        # the next step by calling the function that needs to
-                        # be run. Yes, we are now dividing the commands into
-                        # steps, with each step being when a function requires
-                        # more input. An example dictionary is as follows:
-                        # {'requirements': {'IO': 'I', prompt: 'Enter name:'}}
-                        # Breaking it down, we want input with the prompt to
-                        # the user saying 'Enter name:'
-                        in_thing = None  # Set the input to None
-                        out_thing = None  # Set the output to None
-                        # Set the function requirements to None
-                        requirements_for_func = None
-                        # Set the variable to the requirements given by the
-                        # function that was previously executed
-                        requirements = func_result['requirements']
-                        # If the input/output requirement (IO) is input (I)
-                        if requirements['IO'] == 'I':
-                            # Ask the user for input with the prompt given
-                            # by the function (from the requirements dict)
-                            in_thing = input(requirements['prompt'])
-                            # Set the variable to be returned to the function
-                            # to the input that the user gave
-                            requirements_for_func = in_thing
-                        # Get the name of the function to be executed next
-                        # Which was given by the requirements dictioanry
-                        next_func = func_result['next_func']
-                        # Execute the function and capture the result in case
-                        # we need to execute another function in the next loop
-                        func_result = next_func(requirements_for_func)
-                    except Exception:  # In case anything went wrong
-                        print("Sorry, an error occured")  # Tell the user
-                        break  # Break out of the loop and return to the prompt
+            func(argsplit.process_string(COMMAND_INPUT))
             RUN_THIS_LOOP = True  # Yes, we have executed a command this loop
     if not RUN_THIS_LOOP:  # If a command was not executed this loop
         if JUST_COMMAND == "":  # If the user didn't give a command
