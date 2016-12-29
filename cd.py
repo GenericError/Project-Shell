50f46d613fd9e6786ec615c069033603f1e9e100
Generic Error <GenericError@users.noreply.github.com>
2016-12-29 17:20:17 +1100

    Updated cp
    cp now understands getopt output

--- cd.py
+++ cd.py
@@ -1,33 +1,18 @@
 """ Module that executes the cd command """
 import os  # Importing this module to do directory things
 import argparse
+import getopt
 from shellexceptions import *
 
-def run_command(argument_list):
+def run_command(options, arguments):
     """ Runs the cd command """
-    amount_required = 1
     try:
-        if len(argument_list) >= amount_required:
-            pass
-        else:
-            raise Exception
-    except Exception as e:
-        try:
-            raise FlagOrArgumentNotGivenException
-        except FlagOrArgumentNotGivenException as new_e:
-            new_e.print_error()
-            return None
-
-    new_directory = ""
-    for i in argument_list:
-        if not i.startswith("-"):
-            new_directory = i
-            break
+        new_directory = arguments[0]
+    except Exception:
+        new_directory = "~"
     directory_to_go = os.path.join(os.getcwd(), new_directory)
     try:
-        if new_directory == "":  # If the user put in a nothing or a space
-            raise BlankDirectoryException
-        elif new_directory == "~":  # If the user wants to go to the home folder
+        if new_directory == "~":  # If the user wants to go to the home folder
             os.chdir(os.path.expanduser('~'))  # Goes to the folder
         # If the argument does not give a valid directory
         elif not os.path.isdir(directory_to_go):
