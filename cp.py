248807151fce669b7fa44f8881dff90823fac672
Generic Error <GenericError@users.noreply.github.com>
2016-12-30 12:46:19 +1100

    cp now works
    cp now works with the new method of processing user input

--- cp.py
+++ cp.py
@@ -5,35 +5,25 @@ import os  # Import this for system things
 from shellexceptions import *
 
 
-def run_command(argument_list):
+def run_command(options, arguments):
     """ Function that runs the acutal command based on the user's input """
-    amount_required = 2
-
     try:
-        if len(argument_list) >= amount_required:
-            pass
-        else:
-            raise Exception
-    except Exception as e:
+        source = os.path.abspath(arguments[0])
+        destination = os.path.abspath(arguments[1])
+    except IndexError:
         try:
             raise FlagOrArgumentNotGivenException
-        except FlagOrArgumentNotGivenException as new_e:
-            new_e.print_error()
+        except FlagOrArgumentNotGivenException as e:
+            e.print_error()
             return None
-    
-    source = None
-    destination = None
-    for i in argument_list:
-        if not i.startswith("-"):
-            if source is None:
-                source = i
-            else:
-                destination = i
-
+    verbose = False
+    for option in options:
+        if option[0] in "-v":
+            verbose = True
     try:
         if source == destination:  # If the source and the destination are the same
             raise SourceDestinationAreEqualException
-        elif os.path.isdir(source):  # Im the source argument is a directory
+        elif os.path.isdir(source):  # If the source argument is a directory
             raise SourceArgumentIsADirectoryException
         elif os.path.isfile(source):  # If the source is a file
             if os.path.exists(destination):  # If the destination exists
@@ -44,6 +34,8 @@ def run_command(argument_list):
                 try:  # Try to do the following
                     # Copy the source to the destination with metadata
                     shutil.copy2(source, destination)
+                    if verbose:
+                        print(source, "->", destination)
                 except OSError:  # In case an error occured
                     raise GenericException
             else:  # In case something went wrong
