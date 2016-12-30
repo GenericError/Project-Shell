4de066c59cd78125dde6efb5f84f8a9c658db28e
Generic Error <GenericError@users.noreply.github.com>
2016-12-30 20:50:23 +1100

    Revert "Made code attributes private"
    This reverts commit f1495c60be7eca0bf5a47c6d2e29a9e20504b263.

--- main.py
+++ main.py
@@ -25,17 +25,17 @@ class TerminalColours(object):
     """ Class containing the colour and formatting codes for the terminal """
     def __init__(self):
         # Colour codes
-        self._blue_colour = "\033[94m"
-        self._cyan_colour = "\033[96m"
-        self._darker_cyan_colour = "\033[36m"
-        self._green_colour = "\033[92m"
-        self._yellow_colour = "\033[93m"
-        self._purple_colour = "\033[95m"
-        self._red_colour = "\033[91m"
+        self.blue_colour = "\033[94m"
+        self.cyan_colour = "\033[96m"
+        self.darker_cyan_colour = "\033[36m"
+        self.green_colour = "\033[92m"
+        self.yellow_colour = "\033[93m"
+        self.purple_colour = "\033[95m"
+        self.red_colour = "\033[91m"
         # Formatting codes
-        self._bold_formatting = "\033[1m"
-        self._end_of_colours = "\033[0m"
-        self._underline_formatting = "\033[4m"
+        self.bold_formatting = "\033[1m"
+        self.end_of_colours = "\033[0m"
+        self.underline_formatting = "\033[4m"
 
     def get_colour_code(self, colour_name):
         """ Fetches the colour code for the colour name passed """
