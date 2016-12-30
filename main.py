5894ca868b274826374690eff959d6e86d7b9f73
Generic Error <GenericError@users.noreply.github.com>
2016-12-30 20:45:04 +1100

    Added extra exit and clear branches
    Added extra branches to the exit and clear commands

--- main.py
+++ main.py
@@ -71,24 +71,44 @@ def exit_from_exception():
 
 def exit_command(options, arguments):
     """ Exits the shell """
-    if arguments == []:  # If the arguments dictionary is blank
-        print("Exiting...")  # Tells the user that the program is closing
-        sys.exit()  # Exits the program
-    else:  # Otherwise, do the following
-        # Tell the user that they were ignored
-        print("Arguments", arguments, "were ignored!")
-        print("Exiting...")  # Tells the user that the program is closing
-        sys.exit()  # Exits the program
+    if options == []:
+        if arguments == []:  # If the arguments dictionary is blank
+            print("Exiting...")  # Tells the user that the program is closing
+            sys.exit()  # Exits the program
+        else:  # Otherwise, do the following
+            # Tell the user that they were ignored
+            print("Arguments", arguments, "were ignored!")
+            print("Exiting...")  # Tells the user that the program is closing
+            sys.exit()  # Exits the program
+    else:
+        if arguments == []:
+            print("Options", options, "were ignored!")
+            print("Exiting...")
+            sys.exit()
+        else:
+            print("Options", options, "were ignored!")
+            print("Arguments", arguments, "were ignored!")
+            print("Exiting...")
+            sys.exit()
 
 
 def clear_command(options, arguments):
     """ Clears the shell """
-    if arguments == []:  # If no arguments were provided
-        print(chr(27) + "[2J")  # Clears the shell and returns to the prompt
-    else:  # Otherwise
-        print(chr(27) + "[2J")  # Clears the shell
-        # Tells the user that they were ignored
-        print("Arguments", arguments, "were ignored!")
+    if options == []:
+        if arguments == []:  # If no arguments were provided
+            print(chr(27) + "[2J")  # Clears the shell and returns to the prompt
+        else:  # Otherwise
+            print(chr(27) + "[2J")  # Clears the shell
+            # Tells the user that they were ignored
+            print("Arguments", arguments, "were ignored!")
+    else:
+        if arguments == []:
+            print(chr(27) + "[2J")
+            print("Options", options, "were ignored!")
+        else:
+            print(chr(27) + "[2J")
+            print("Options", options, "were ignored!")
+            print("Arguments", arguments, "were ignored!")
 
 # The following dictionary has the list of the commands, and the functions that
 # are called when the command is searched for in the dictionary
