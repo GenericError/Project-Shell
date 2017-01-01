53cc6235c79246ad4ecc89a2ed2b5a4002eeef99
Generic Error <GenericError@users.noreply.github.com>
2017-01-01 14:01:30 +1100

    Fixed obvious errors
    Fixed obvious errors by removing a piece of unnecessary code

--- printworkingdir.py
+++ printworkingdir.py
@@ -4,17 +4,5 @@ import os
 
 def run_command(options, arguments):
     """ Function that runs the pwd command """
-    amount_required = 0
-    try:
-        if len(argument_list) >= amount_required:
-            pass
-        else:
-            raise Exception
-    except Exception as e:
-        try:
-            raise FlagOrArgumentNotGivenException
-        except FlagOrArgumentNotGivenException as new_e:
-            new_e.print_error()
-            return None
 
     print(os.path.abspath(os.getcwd()))
