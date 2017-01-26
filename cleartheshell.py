e109e3cbd464a278c53350dca5c9d12040185318
Generic Error <GenericError@users.noreply.github.com>
2017-01-26 18:26:10 +1100

    Added docs for clear
    Added man documentation to the clear command

--- cleartheshell.py
+++ cleartheshell.py
@@ -1,5 +1,10 @@
 """ The module that contains the clear command code """
 
+MAN_DOC = """clear - make the terminal blank
+Usage: clear
+
+The command will make the terminal blank, resulting in a single prompt being on the screen.
+NOTE: The command is a bit buggy at the moment, resulting in the prompt being located at random vertical locations. If you have a fix, please fork us."""
 
 def run_command(options, arguments):
     """ Clears the shell """
