3e9faf47e6b3f45734bc9b60ed9c218bd18a4c5e
Generic Error <GenericError@users.noreply.github.com>
2017-01-28 09:58:28 +1100

    Unstable incomplete commit
    This commit is incomplete; do not use it for testing purposes

--- project-shell-testing.py
+++ project-shell-testing.py
@@ -40,5 +40,58 @@ class TestClearCommand(unittest.TestCase):
         import cleartheshell
 
 
+class TestCpCommand(unittest.TestCase):
+    """ This module tests the cp command
+    A most of these just check that the code handles the exception properly"""
+
+    def setUp(self):
+        """ Sets up some things for the tests """
+        self.TestFile = "/tmp/project-shell-cp.testfile"
+        try:
+            os.remove("/tmp/project-shell-cp/project-shell-cp.testfile")
+            os.remove(self.TestFile)
+        except:
+            pass
+        self.TestFileObject = open(file=self.TestFile, mode='w')
+        self.ThingToWrite = "This is just a test"
+        self.TestFileObject.write(self.ThingToWrite)
+        self.TestFileObject.close()
+        if not os.path.exists("/tmp/project-shell-cp"):
+            os.mkdir("/tmp/project-shell-cp")
+
+
+    def tearDown(self):
+        """ Destroys things from previous tests """
+        self.TestFileObject.close()
+
+
+    def test_no_arguments(self):
+        """ cp """
+        cp.run_command([], [])
+
+    def test_missing_argument(self):
+        """ cp foobar """
+        cp.run_command([], ['foobar'])
+
+    def test_equal_source_and_directory(self):
+        """ cp foobar foobar """
+        cp.run_command([], ['foobar', 'foobar'])
+
+    def test_source_is_a_directory(self):
+        """ cp /home foobar """
+        cp.run_command([], ['/home', 'foobar'])
+
+    def test_destination_is_a_directory(self):
+        """ cp /tmp/project-shell-cp.testfile /tmp """
+        cp.run_command([], [self.TestFile, "/tmp/project-shell-cp"])
+
+    def test_copy_file(self):
+        """ cp /tmp/project-shell-cp.testfile /tmp/project-shell-cp/project-shell-cp.testfile """
+        cp.run_command([], [self.TestFile, "/tmp/project-shell-cp/project-shell-cp.testfile"])
+        new_file = open(file="/tmp/project-shell-cp/project-shell-cp.testfile", mode="r")
+        new_file_contents = new_file.readlines()[0]
+        self.assertEqual(new_file_contents, "This is just a test")
+        new_file.close()
+
 if __name__ == '__main__':
     unittest.main()
