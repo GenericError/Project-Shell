11caf575da50d91b72f4255f16946c88d3d0628d
Generic Error <GenericError@users.noreply.github.com>
2017-01-27 13:35:05 +1100

    Revert "Added tests for cleartheshell"
    This reverts commit 05ab78f9b7f631002716d6ebb3546d24538db580.

--- project-shell-testing.py
+++ project-shell-testing.py
@@ -7,7 +7,6 @@ except ImportError:
 
 import os
 import cd
-import cleartheshell
 
 class TestCdCommand(unittest.TestCase):
     """ This module tests the cd command """
@@ -30,22 +29,6 @@ class TestCdCommand(unittest.TestCase):
         self.assertEqual(path_wanted, directory_after_operation,
                          msg="cd did not change the directory")
 
-
-class TestClearTheShellCommand(unittest.TestCase):
-    """ This module doesn't really 'test' anything -
-    in fact it will only fail if there is a syntax error """
-
-    def test_clear(self):
-        cleartheshell.run_command([], [])
-
-    def test_clear_with_redundant_opts(self):
-        cleartheshell.run_command(['-f'], [])
-
-    def test_clear_with_redundant_args(self):
-        cleartheshell.run_command([], ['foobar'])
-
-    def test_clear_with_redundant_opts_and_args(self):
-        cleartheshell.run_command(['-f'], ['foobar'])
 
 
 if __name__ == '__main__':
