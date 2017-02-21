11e24b401689ab839751e5fd8eb0557ae3be939a
Generic Error <GenericError@users.noreply.github.com>
2017-02-21 12:10:52 +1100

    Added append mode
    Added appending to the permission_aware_open function

--- permission_awareness.py
+++ permission_awareness.py
@@ -8,26 +8,34 @@ def check_existence(path):
     """ Returns True if the path exists """
     return os.access(path, os.F_OK)
 
+
 def check_read_permission(path):
     """ Returns True if the user/group can read the path """
     return os.access(path, os.R_OK)
+
 
 def check_write_permission(path):
     """ Returns True if the user/group can write to the path """
     return os.access(path, os.W_OK)
 
+
 def check_execute_permission(path):
     """ Returns True if the user/group can execute the path """
     return os.access(path, os.X_OK)
 
+
 def permission_aware_open(path, mode):
-    """ Returns a file object, checking for appropriate permissions before doing so """
+    """ Returns a file object, checking for appropriate
+    permissions before opening the file """
     ok = False
     if check_existence(path):
         if mode == "r":
             if check_read_permission(path):
                 ok = True
         elif mode == "w":
+            if check_write_permission(path):
+                ok = True
+        elif mode == "a":
             if check_read_permission(path):
                 if check_write_permission(path):
                     ok = True
