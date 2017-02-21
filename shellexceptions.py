ce494b0c2348cec600d594449cac0e1ab59892db
Generic Error <GenericError@users.noreply.github.com>
2017-02-21 12:36:09 +1100

    Added lack of existence exceptions

--- shellexceptions.py
+++ shellexceptions.py
@@ -144,3 +144,17 @@ class CommandNotFoundException(CustomBaseException):
     def __init__(self, command_name=""):
         super().__init__()
         self.error_message = construct_error_message(supplied_information=command_name, error_string="The command $VAR$ could not be found.", default_error_string="The command could not be found.")
+
+
+class SourceDoesNotExistException(CustomBaseException):
+    """ Raise if the source argument does not exist on the system """
+    def __init__(self, command_name=""):
+        super().__init__()
+        self.error_message = construct_error_message(supplied_information=command_name, error_string="The source argument $VAR$ does not exist.", default_error_string="The source argument does not exist.")
+
+
+class DestinationDoesNotExistException(CustomBaseException):
+    """ Raise if the destination argument does not exist on the system """
+    def __init__(self, command_name=""):
+        super().__init__()
+        self.error_message = construct_error_message(supplied_information=command_name, error_string="The destination argument $VAR$ does not exist.", default_error_string="The destination argument does not exist.")
